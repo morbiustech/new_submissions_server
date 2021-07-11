545cd2d747efc49212286bc0f50b59abb8831cfa
morbiustech <mithilesh@morbius.co>
2021-07-11 21:10:03 +0530

    started working on update student functionality

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -48,6 +48,32 @@ exports.findOne = (req, res) => {
 
 };
 
+exports.update = (req, res) => {
+
+  if (!req.body) {
+      return res.status(400).send({
+        message: "Data to update can not be empty!"
+      });
+    }
+  
+    const id = req.params.id;
+  
+    Students.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+      .then(data => {
+        if (!data) {
+          res.status(404).send({
+            message: `Cannot update Students with id=${id}. Maybe Student was not found!`
+          });
+        } else res.send({ message: "Student was updated successfully." });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating Student with id=" + id
+        });
+      });
+
+};
+
 exports.findAll = (req, res) => {
 
 
