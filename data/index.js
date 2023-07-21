ee7a1ce1b641453668d822094186a777df858bd6
mithilesh <mithilesh@morbius.co>
2023-07-21 17:10:31 +0530

    fixed email not working issue

---
+++ data/index.js
@@ -0,0 +1,8 @@
+const data = {
+    MAILGUN_API_KEY:"92fbba3340d55791b469754ed108fa9a-53c13666-e46ce901",
+    MAILGUN_DOMAIN:"notifications.morbius.co",
+    PUBLIC_KEY:"pubkey-e07a650a7d433b1059ff404a2d74e64c"
+}
+module.exports = {
+    data
+}
